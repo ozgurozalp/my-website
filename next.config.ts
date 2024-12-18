5fd5e8533f9b75871e3b2e6eac9a37249b3508e4
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2024-12-18 14:57:41 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import createMDXPlugin from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
+const oldBLogs = [
+  "/node-js-express-uygulamasi-nasil-dockerize-edilir",
+  "/kubernetes-nedir-ve-neden-kullanilir",
+  "/windows-bilgisayarda-php-calistirma",
+  "/guzzle-http-nasil-kullanilir",
+  "/deno-js-nedir",
+  "/react-nedir",
+  "/wordpress-nedir-nasil-kurulur",
+  "/php-nedir-ne-amacla-kullanilir",
+  "/vuejs-nedir-neden-kullanmaliyiz",
+];
+
 const withMDX = createMDXPlugin({
   extension: /\.mdx?$/,
   options: {
@@ -19,6 +31,11 @@ export default withMDX({
         destination: "/blog/category/:slug",
         permanent: true,
       },
+      ...oldBLogs.map((oldBlog) => ({
+        source: oldBlog,
+        destination: "/blog" + oldBlog,
+        permanent: true,
+      })),
     ];
   },
 });
