6bb705afde2228f6e7b19847df1cae253b5e1b42
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2024-12-18 12:40:17 +0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,10 @@ export default function NotFoundPage() {
   return (
     <div
       style={{ height: "calc(100vh - 5rem)" }}
-      className="h-dvh flex items-center justify-center"
+      className="h-dvh flex flex-col gap-4 items-center justify-center"
     >
-      <h1 className={"text-6xl font-mono"}>404</h1>
-      <h2>Page Not Found</h2>
+      <h1 className={"text-7xl font-mono"}>404</h1>
+      <h2 className="text-5xl">Page Not Found</h2>
     </div>
   );
 }
